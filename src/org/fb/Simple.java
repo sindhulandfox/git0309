f301ea8dfea49d813cfcd965212546e169b9d268
sindhulandfox <sindhu.tmg@gmail.com>
2022-09-03 14:43:49 +0530

    hello

---
+++ src/org/fb/Simple.java
@@ -0,0 +1,16 @@
+package org.fb;
+
+public class Simple {
+	
+	public void empId() {
+		System.out.println("enter employee id");
+
+	}
+	public void empname() {
+		System.out.println("enter employee name :+name");
+		
+
+	}
+	
+
+}
